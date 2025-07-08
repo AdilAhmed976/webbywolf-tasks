01cd6fb58d5bd820ff838d3e05e0b4b74d591208
AdilAhmed976 <adilahmedah976@gmail.com>
2025-07-08 13:52:43 +0530

    fix : UI Updates

--- src/sections/PolicyDocumentView.tsx
+++ src/sections/PolicyDocumentView.tsx
@@ -11,7 +11,7 @@ import {
 import Image from "next/image";
 import { useReactToPrint } from "react-to-print";
 import { Button } from "@/components/ui/button";
-import { Printer } from "lucide-react";
+import { SaveIcon } from "lucide-react";
 
 interface ChannelPartnerDetails {
   company_name: string;
@@ -350,9 +350,12 @@ const PolicyDocumentView = () => {
   });
   return (
     <div className="max-w-3xl mx-auto p-4 bg-gray-50 min-h-screen">
-      <Button className="cursor-pointer" onClick={reactToPrintFn}>
-        <Printer /> Print
-      </Button>
+      <div className="flex items-center justify-center p-3">
+        <Button className="cursor-pointer" onClick={reactToPrintFn}>
+          Save As PDF
+          <SaveIcon />
+        </Button>
+      </div>
       <Card className="bg-white shadow-lg gap-2">
         <div ref={contentRef}>
           <style>
